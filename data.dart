7b7fcc97cc2a0aef97b9597f0b9703656bc4c643
Zakaria Morchid <morchid.zakariaa@gmail.com>
2025-04-30 21:49:10 +0200

    Update data.dart

--- data.dart
+++ data.dart
@@ -5,37 +5,9 @@ const Categories = [
 
 const Subjects = [
   Subject(
-    id: 'm1',
-    categories: ['c1'],
     title: 'Calcule mental',
-    affordability: Affordability.affordable,
-    complexity: Complexity.simple,
-    imageUrl:
-        'https://cdn.pixabay.com/photo/2019/10/23/14/13/school-4571739_1280.jpg',
-    duration: 20,
     summary:
         "Résoudre des problèmes de mathématiques en utilisant des techniques de calcul mental.",
     exercices: [],
-    isGlutenFree: false,
-    isVegan: true,
-    isVegetarian: true,
-    isLactoseFree: true,
-  ),
-  Subject(
-    id: 'm2',
-    categories: ['c1'],
-    title: 'Algèbre',
-    affordability: Affordability.affordable,
-    complexity: Complexity.simple,
-    imageUrl:
-        'https://cdn.pixabay.com/photo/2017/06/16/08/19/math-2408142_1280.jpg',
-    duration: 20,
-    summary:
-        "Résoudre des problèmes de mathématiques en utilisant des techniques de calcul mental.",
-    exercices: [],
-    isGlutenFree: false,
-    isVegan: true,
-    isVegetarian: true,
-    isLactoseFree: true,
   ),
 ];
