1c916f51d284b8ebd7889d89842bde3d58728373
Zakaria Morchid <morchid.zakariaa@gmail.com>
2025-05-01 01:51:02 +0200

    Update data.dart

--- data.dart
+++ data.dart
@@ -1,13 +1,7 @@
+Subjects:
+  - title: "Calcule mental"
+    summary: "Résoudre des problèmes de mathématiques en utilisant des techniques de calcul mental."
+    exercices:
+      - "ex 1"
+      - "ex 2"
 
-const Categories = [
-  Category(id: 'c1', title: 'Math', color: Colors.purple),
-];
-
-const Subjects = [
-  Subject(
-    title: 'Calcule mental',
-    summary:
-        "Résoudre des problèmes de mathématiques en utilisant des techniques de calcul mental.",
-    exercices: ['ex 1', 'ex 2'],
-  ),
-];
